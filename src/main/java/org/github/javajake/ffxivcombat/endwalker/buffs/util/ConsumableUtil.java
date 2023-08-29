c485ce1b679529de8ee83faafc9f71276e4a52fa
Jacob Godserv <jacobgodserv@gmail.com>
2023-08-28 23:55:03 -0400

    Food -> consumable

--- src/main/java/org/github/javajake/ffxivcombat/endwalker/buffs/util/ConsumableUtil.java
+++ src/main/java/org/github/javajake/ffxivcombat/endwalker/buffs/util/ConsumableUtil.java
@@ -6,28 +6,29 @@ import org.github.javajake.ffxivcombat.endwalker.buffs.StatMultiplier;
 import org.github.javajake.ffxivcombat.endwalker.character.PlayableCharacter;
 
 /**
- * Provides convenient tools for dealing with stat adjustment buffs.
+ * Provides convenient tools for dealing with food buffs.
  */
-public final class StatAdjustmentUtil {
-  private StatAdjustmentUtil() {
+public final class ConsumableUtil {
+  private ConsumableUtil() {
   }
 
   /**
-   * Create a new stat adjustment buff that represents the way that a food stat adjustment works.
+   * Create a new stat adjustment buff that represents the way that a consumable stat adjustment
+   * works.
    * <ol>
    *   <li>The stat in question (say, strength) is retrieved from the character.</li>
-   *   <li>The food's stat multiplier is applied to find the maximum adjustment.</li>
+   *   <li>The consumable's stat multiplier is applied to find the maximum adjustment.</li>
    *   <li>The maximum adjustment is compared against the capped adjustment. Whichever is lower
    *   wins.</li>
    *   <li>A new stat adjustment buff with this new capped amount.</li>
    * </ol>
    *
    * @param character     the character whose stats are being buffed
-   * @param multiplier    the food buff at its maximum potential
-   * @param adjustmentCap the cap on how much the food buff can
+   * @param multiplier    the consumable buff at its maximum potential
+   * @param adjustmentCap the cap on how much the consumable buff can
    * @return the stat adjustment calculated from the capped stat multiplier
    */
-  public static StatAdjustment capFoodBuff(
+  public static StatAdjustment createConsumableBuff(
       final PlayableCharacter character,
       final StatMultiplier multiplier,
       final int adjustmentCap) {
